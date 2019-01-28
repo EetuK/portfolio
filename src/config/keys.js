aabf0567360068fb6e15fa2d13197aca810e0117
Eetu <35661367+EetuK@users.noreply.github.com>
2019-01-28 20:07:31 +0200

    Update keys.js

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,7 +1,6 @@
-console.log("Node envs:");
-console.log(process.env);
-if (process.env.NODE_ENV === "production") {
+module.exports = require("./keys_dev");
+/*if (process.env.NODE_ENV === "production") {
   module.exports = require("./keys_prod");
 } else {
   module.exports = require("./keys_dev");
-}
+}*/
